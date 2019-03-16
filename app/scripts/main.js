3bee45cc8a99f78535e74ae4b9286b15c7ed0767
niinoolopes <niinoolopes0@gmail.com>
2019-03-16 13:27:40 -0300

    update projeto

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,23 @@ if( window.innerWidth > 768 ){
   const heightAside = document.querySelector('.aside__menu').offsetHeight;
   document.querySelector('.main__page').style.height = `${heightAside}px`; 
 }
-// const btn = document.querySelector('#tbn-menu')
-// btn.addEventListener('click',function(){
-//   document.querySelector('.asideMain').classList.toggle('ativo')
-// })
+const clickMobile = (seletor) => {
+  const element = document.querySelector(`${seletor}`)
+  const element_Dataset = element.dataset.menu
+  console.log(element);
+  console.log(element_Dataset);
+
+  element.addEventListener('click', () => {
+    /*
+    const getElement = document.querySelector(`.${element_Dataset}`) 
+    getElement.classList.toggle('ativo')
+    */
+   document.querySelector(`.${element_Dataset}`).classList.toggle('ativo')
+  })
+
+
+}
+
+clickMobile('[data-menu="main__navegacao"]')
+clickMobile('[data-menu="main__notificacao"]')
+
